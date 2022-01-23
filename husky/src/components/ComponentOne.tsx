a276d8f03005c94434aa2974ed0676dd70d9e5a0
narek.malkhasyan <narek.malkhasyan@arctx.co>
2022-01-23 22:07:04 +0400

    working example without typescript

--- husky/src/components/ComponentOne.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-
-export const ComponentOne = () => {
-    return <h1>Greetings</h1>
-}
--- husky/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { ComponentOne } from './components/ComponentOne'
-
-ReactDOM.render(<ComponentOne />, document.getElementById('app'))
