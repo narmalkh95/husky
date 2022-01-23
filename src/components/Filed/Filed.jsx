37beb890f766693fb702fbe89fdd86f4a41958df
narek.malkhasyan <narek.malkhasyan@arctx.co>
2022-01-23 23:22:09 +0400

    project init

---
+++ src/components/Filed/Filed.jsx
@@ -0,0 +1,30 @@
+function Field({
+  label,
+  id,
+  type,
+  placeholder,
+  required,
+  autoComplete,
+  value,
+  onChange,
+}) {
+  return (
+    <div className="FormRow">
+      <label htmlFor={id} className="FormRowLabel">
+        {label}
+      </label>
+      <input
+        className="FormRowInput"
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        required={required}
+        autoComplete={autoComplete}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default Field;
