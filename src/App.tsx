2d2682016c15c770bc9b46101bfd6dddfb0f71e4
narek.malkhasyan <narek.malkhasyan@arctx.co>
2022-01-24 19:50:51 +0400

    qwe

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 'qweqwe';
+
+  console.log(a);
   return (
     <div className="App">
       <header className="App-header">
